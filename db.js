7d0b12ed34598f80a4880cbf195f1a9585f9a038
asha <ashatirumani1234@gmail.com>
2025-07-28 14:59:47 +0530

    Update db.js to use DATABASE_URL

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const pool = new Pool();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Needed for Render PostgreSQL
+  },
+});
+
  
 module.exports = pool; 
