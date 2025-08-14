7bcf6f75be14dd3c02ecffe125f67a1d7d69efc9
GANGISHETTI VIKRAM <138607180+vikramsidharth@users.noreply.github.com>
2025-08-14 12:44:44 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,13 +1,22 @@
 const { Pool } = require('pg');
 
-// Database configuration for local testing
-const pool = new Pool({
-  user: 'postgres',           // e.g. 'postgres'
-  host: 'localhost',
-  database: 'OCFA',       // e.g. 'ocfa_db'
-  password: '123456',   // e.g. 'yourpassword'
-  port: 5432,                     // default PostgreSQL port
-  ssl: false                      // since you're not using SSL
-});
+const isProduction = process.env.NODE_ENV === 'production';
+
+const pool = new Pool(
+  isProduction
+    ? {
+        connectionString:
+          'postgresql://ocfa_rdea_user:bo9wsbOddjXXjIeOOhWLYLxLGG2U8DLn@dpg-d23inammcj7s739egja0-a.oregon-postgres.render.com/ocfa_rdea',
+        ssl: { rejectUnauthorized: false }
+      }
+    : {
+        user: 'postgres',          // local user
+        host: 'localhost',         // local host
+        database: 'OCFA',          // local DB name
+        password: '123456',        // local password
+        port: 5433,                 // local port
+        ssl: false
+      }
+);
 
 module.exports = pool;
