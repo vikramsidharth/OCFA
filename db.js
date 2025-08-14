2f215197ffff7fa84b1fc78878ef2fda0b9c360e
GANGISHETTI VIKRAM <138607180+vikramsidharth@users.noreply.github.com>
2025-08-14 13:05:20 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,8 +5,11 @@ const isProduction = process.env.NODE_ENV === 'production';
 const pool = new Pool(
   isProduction
     ? {
-        connectionString:
-          'postgresql://ocfa_rdea_user:bo9wsbOddjXXjIeOOhWLYLxLGG2U8DLn@dpg-d23inammcj7s739egja0-a.oregon-postgres.render.com/ocfa_rdea',
+        user: 'ocfa_rdea_user',
+        host: 'dpg-d23inammcj7s739egja0-a.oregon-postgres.render.com',
+        database: 'ocfa_rdea',
+        password: 'bo9wsbOddjXXjIeOOhWLYLxLGG2U8DLn',
+        port: 5432,
         ssl: { rejectUnauthorized: false }
       }
     : {
